0c54a254433c002e66ad4592a80b3c700937157f
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-02-17 15:46:50 +0530

    split init with input and output doc

---
+++ split.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#    _____       ___ __               
+#   / ___/____  / (_) /_  ____  __  __
+#   \__ \/ __ \/ / / __/ / __ \/ / / /
+#  ___/ / /_/ / / / /__ / /_/ / /_/ / 
+# /____/ .___/_/_/\__(_) .___/\__, /  
+#     /_/             /_/    /____/   
+
+# usage: python split.py <destination>/<filename>.<ext> <number of lines limit per file>
+# output: files with contents of parent file
+#			writing <destination>/<filename>-part1.<ext>
+#			writing <destination>/<filename>-part2.<ext> .. so on
+
