6f75691824c3a232f5ec557abfaa3f00aba119e7
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-02-17 15:32:17 +0530

    URI fetch established

--- howlong.py
+++ howlong.py
@@ -11,4 +11,48 @@
 # usage: python howlong.py [location1] [location2]
 # output: <distance (km)> (newline) <time (days, hrs, mins)>
 
+class HowLong:
+	'''
+	data members
+	'''
+	LOCATION1 = ""
+	LOCATION2 = ""
+	MATRIXURI = "https://maps.googleapis.com/maps/api/distancematrix/json"
 
+	'''
+	@name __init__ (constructor)
+	@params location1, location2
+	'''
+	def __init__(self, location1, location2):
+		self.LOCATION1 = location1.replace(" ", "+")
+		self.LOCATION2 = location2.replace(" ", "+")
+
+	'''
+	@name execute
+	@algorithm fetch data from distancematrix uri with locations params and return distance
+			   and time to travel from one place to another.
+	'''
+	def execute(self):
+		# import requests library for easy access
+		import requests
+		uridata = {
+			'units':'metric',
+			'origins':self.LOCATION1,
+			'destinations':self.LOCATION2
+		}
+		response = requests.get(self.MATRIXURI, params=uridata)
+		print response.json()
+
+
+def main():
+	import sys
+	try:
+		location1, location2 = sys.argv[1], sys.argv[2]
+	except IndexError:
+		raise SystemExit('Please provide both the locations')
+	
+	howlong = HowLong(location1,location2)
+	howlong.execute()
+
+if __name__ == '__main__':
+	main()
